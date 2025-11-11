7a127c3bc46fdb52cf291f86348b3b9de9c75f49
aylenbustamante <166763845+aylenbustamante@users.noreply.github.com>
2025-11-11 01:48:28 -0300

    Update panqueques.js

--- tpfinalparte2JULIVIERNES7/panqueques.js
+++ tpfinalparte2JULIVIERNES7/panqueques.js
@@ -1,29 +1,27 @@
 class Panqueques {
-    /**
-     * Gestiona el panqueque que cae (creación, tipo, color, velocidad y posición).
-     */
+   
     constructor(width) {
         this.ancho = 80;
         this.alto = 10;
         this.velocidad = 3;
         this.x = 0;
         this.y = 0;
-        this.actual = {}; // Contiene {tipo: 'NORMAL'/'ROJO', color: p5.Color}
+        this.actual = {};
         this.width = width;
     }
 
     reiniciar() {
         this.x = random(this.ancho / 2, this.width - this.ancho / 2);
         this.y = -this.alto;
-        this.velocidad += 0.05; // Aumenta ligeramente la velocidad para dificultad
+        this.velocidad += 0.05; 
 
-        // 20% de probabilidad de ser rojo
+        //las probabilidades de q sea rojo 
         if (random() < 0.20) { 
             this.actual.tipo = 'ROJO';
             this.actual.color = color(255, 50, 50); // Rojo brillante
         } else {
             this.actual.tipo = 'NORMAL';
-            this.actual.color = color(random(200, 255), random(100, 150), 50); // Color normal
+            this.actual.color = color(random(200, 255), random(100, 150), 50); // Colores naranjas
         }
     }
 
@@ -32,14 +30,14 @@ class Panqueques {
     }
 
     verificarColision(jugadorX) {
-        // 1. Calcula la altura de colisión (parte superior de la torre/jugador)
+        
         const alturaTorre = estado.pilaPanqueques.length * this.alto;
         const alturaColisionY = jugador.y - jugador.alto / 2 - alturaTorre;
 
         if (this.y >= alturaColisionY) {
             // 2. Verifica Colisión horizontal con la posición actual del jugador
             if (this.x > jugadorX - this.ancho / 2 && this.x < jugadorX + this.ancho / 2) {
-                // Colisión Exitosa
+                // torre ganador
                 if (this.actual.tipo === 'ROJO') {
                     estado.perder();
                 } else {
